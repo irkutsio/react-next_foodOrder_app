a6cd7a9141816c5d2849b9d12b7da099573e4d84
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2024-05-19 20:59:31 +0300

    created

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'NextJS Course App',
+  description: 'Your first NextJS app!',
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
